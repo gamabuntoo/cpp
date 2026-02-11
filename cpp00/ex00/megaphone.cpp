7e38c7a25fdcbe26d22ab4b9afd7e5ba7fe56724
Guillaume Le Batard <gule-bat@f2r5s22.clusters.42paris.fr>
2026-02-11 17:19:32 +0100

    cpp00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+// #include "phonebook.hpp"
+
+int	main(int argc, char **argv)
+{
+	std::string stri;
+	// Phonebook phone;
+
+	if (argc == 1)
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
+	for (int i = 1; i < argc; i++)
+	{
+		stri = argv[i];
+		for (int j = 0; j < (int)stri.size(); j++)
+		{
+			if (std::islower(stri[j]))
+				std::cout << (char)(stri[j] - 32);
+			else
+				std::cout << stri[j];
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
---
+++ cpp00/ex01/phonebook.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include "phonebook.hpp"
+
+// void	add_contact(Phonebook *book)
+// {
+// 	(void)book;
+// 	std::cout << "adddddd" << std::endl;
+// }
+
+// void	search_contact(Phonebook *book)
+// {
+// 	(void)book;
+// 	std::cout << "searchh" << std::endl;
+// }
+int	main(void)
+{
+	std::string line;
+	Phonebook book;
+	int i;
+
+	i = 0;
+	book.size = 0;
+	book.Infos[i].Init_empty();
+	while (i < 9)
+	{
+		std::cout << "\n[\tPHONEBOOK\t]\n\nI- CMD: ADD, SEARCH, EXIT\n\n~ : ";
+		std::getline(std::cin, line);
+		if (!(std::cin))
+			return (1);
+		if (line.empty())
+			continue;
+		std::cout << std::endl;
+		if (line == "ADD")
+		{
+			if (book.add_contact(&i) == 1)
+				i++;
+		}
+		else if (line == "SEARCH")
+		{
+			book.search_contact();
+			// continue ;
+		}
+		else if (line == "EXIT")
+			return (1);
+	}
+	return (0);
+}
